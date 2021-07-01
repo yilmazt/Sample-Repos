c98041ad10b859d66a3ef9294a38e1953d896216
Tolunay Yilmaz <toluu.95@hotmail.com>
2021-07-01 22:08:42 +0300

    Removed some code

--- script.py
+++ script.py
@@ -1,19 +1,6 @@
-import math
-import os
-import sys
-from os import rename
-
 import requests
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-
-    greeting = "hello {}".format(who_to_greet)
-    return greeting
 
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
