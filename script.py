c88c4433a2d46f24dc5cd10e9f28ab7428c71696
Tolunay Yilmaz <toluu.95@hotmail.com>
2021-07-01 22:06:54 +0300

    Initial Commit

---
+++ script.py
@@ -0,0 +1,19 @@
+import math
+import os
+import sys
+from os import rename
+
+import requests
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+
+    greeting = "hello {}".format(who_to_greet)
+    return greeting
+
+
+r = requests.get("https://coreyms.com")
+print(r.status_code)
