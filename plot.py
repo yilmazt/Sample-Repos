a2bd3b9af02bcefec0ed8397cfa17cf5dc7fca78
yilmazt <tolunay.yilmaz@boun.edu.tr>
2021-07-02 20:08:07 +0300

    son

---
+++ plot.py
@@ -0,0 +1,20 @@
+from matplotlib import pyplot as plt
+
+dev_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+
+dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752]
+
+plt.plot(dev_x, dev_y)
+
+
+plt.title("Median Salary(USD)")
+plt.xlabel("Ages")
+plt.ylabel("Median Salary(USD")
+
+
+plt.show()
+# Median Python Developer Salaries by Age
+
+py_dev_y = [45372, 48876, 53850, 57287, 63016, 65998, 70003, 70000, 71496, 75370, 83640]
+
+plt.plot(py)
